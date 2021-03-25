95b5bbe8d3b850fe2f6dbd64159bcc18e3dd794c
otawfik0789 <79497773+otawfik0789@users.noreply.github.com>
2021-03-25 00:04:30 -0400

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,16 @@
+function setup() {
+  createCanvas(800,400);
+  createSprite(400, 200, 50, 50);
+}
+
+function draw() {
+  background(255,255,255);  
+  drawSprites();
+}
+
+function mouseDragged() {
+  matter.Body.setPosition(hero.body)
+
+  constructort(x,y,r)
+  
+}
